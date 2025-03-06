6a2f1d8667169701ee92576eb1b5bfde0c9f6588
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-06 14:33:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import history from 'connect-history-api-fallback'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'html-history-fallback',
+      configureServer(server) {
+        server.middlewares.use(history());
+      }
+    }
+  ]
 })
