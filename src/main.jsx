24d8a20b6bf55b97021dcce07cb449232d436e52
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-06 15:00:37 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './SiteWeb/App';
 import { theme } from './theme';
-import { HashRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <HashRouter hashType="noslash" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+  <MemoryRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </HashRouter>
+  </MemoryRouter>
 );
