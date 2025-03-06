921d163ea47f8136499ba12f68dc5c35751ab740
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-06 15:04:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './SiteWeb/App';
 import { theme } from './theme';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <MemoryRouter>
+  <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </MemoryRouter>
+  </BrowserRouter>
 );
