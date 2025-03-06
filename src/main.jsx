2b36d335be6e5a4a0f9b013dce834445b14c96eb
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-06 16:32:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
 import App from './SiteWeb/App';
 import { theme } from './theme';
-import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter hashType="noslash">
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
