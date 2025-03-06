812261992d07935fe8bcaaab4f2655cf8b0018da
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-06 15:12:54 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './SiteWeb/App';
 import { theme } from './theme';
-import { StaticRouter } from 'react-router-dom/server';
-
-const location = window.location.pathname; // On récupère l'URL actuelle
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <StaticRouter location={location}>
+  <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </StaticRouter>
+  </BrowserRouter>
 );
