a84062c7f9533efdf4348a8ebf426d55dc207e26
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-06 14:48:02 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './SiteWeb/App';
 import { theme } from './theme';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // Remplacez BrowserRouter par HashRouter
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-  <ThemeProvider theme={theme}>
+  <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-</BrowserRouter>
+  </HashRouter>
 );
