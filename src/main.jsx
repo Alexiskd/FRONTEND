9ac54f193146070d33b4d3e50b0062a9b9b922ff
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-11 17:22:16 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
-import App from './SiteWeb/App'; // Par exemple, si App est défini ici
+import App from './SiteWeb/App'; // App est importé ici comme export par défaut
 import { theme } from './theme';
 
 const rootElement = document.getElementById('root');
