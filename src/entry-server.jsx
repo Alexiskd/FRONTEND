5526692d3d5149b3dd9dd03243b821cb35006940
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-11 17:44:12 +0100

    Update entry-server.jsx

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,7 +1,7 @@
 // src/entry-server.jsx
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import App from './SiteWeb/App'; // Assurez-vous que ce fichier exporte "default"
+import App from './SiteWeb/App'; // Vérifiez que App est exporté par défaut
 
 export function render(url) {
   const appHtml = renderToString(<App />);
