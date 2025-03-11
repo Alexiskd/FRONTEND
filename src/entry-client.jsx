0ed1dd1252333dea52a545dc1365471d9f320eeb
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-11 17:22:58 +0100

    Update entry-client.jsx

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,6 +1,6 @@
 // src/entry-client.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './main.jsx'; // Import par défaut
+import App from './SiteWeb/App';
 
 ReactDOM.hydrate(<App />, document.getElementById('root'));
