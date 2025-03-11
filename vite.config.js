ce6a8f238f09859544566347452414a6fa0f44b8
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-11 17:33:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: true, // Active les source maps pour déboguer le code minifié
+  },
+});
